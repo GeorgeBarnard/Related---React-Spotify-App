8988ddea6923675d99942078c86f714758ea99d4
George Barnard <george_barnard@ymail.com>
2018-02-26 21:10:27 +0000

    changed uri re-direct aadded new secret

--- src/constants/app_secrets.js
+++ src/constants/app_secrets.js
@@ -3,8 +3,8 @@ let prod = true;
 const scopes = "user-read-private+user-read-email+playlist-read-private+user-top-read+user-read-recently-played";
 const redirectURI = prod ? "https://calm-fjord-61683.herokuapp.com/" : "http://localhost:8080/";
 
-export const clientID = "28c0d2a90c924223a48b18dc0801c512";
-export const clientSecret = "28af750c1a7446168a886c1e9f9af7e0";
+export const clientID = "3dbf0bf935864a60b4dd00aec0930702";
+export const clientSecret = "3c139e27cf864effadacc48ce2133ffd";
 export const spotifyWebApiURL = `https://accounts.spotify.com/authorize/?client_id=${clientID
 }&response_type=token&redirect_uri=${redirectURI}&scope=${scopes}`;
 export const spotifyProfileURL = "https://api.spotify.com/v1/me?access_token=";
